3355d6def74978f95ad57b35c6e62a1c699683ee
Minkyoung Kim <mk0218@postech.ac.kr>
2022-09-26 09:15:33 +0000

    quick sort implemented

---
+++ algorithm_practices/exam_grades.py
@@ -0,0 +1,12 @@
+import sys
+
+if (score := int(sys.stdin.readline())) >= 100:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
