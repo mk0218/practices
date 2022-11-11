401fb6f3f44ab129f3446b92155b876c52367d70
Minkyoung Kim <mk0218@postech.ac.kr>
2022-11-11 10:03:46 +0000

    Baekjoon #12100 2048 Game (easy)

--- algorithm_practices/bj_12100_2048ez.py
+++ algorithm_practices/bj_12100_2048ez.py
@@ -1,4 +1,4 @@
-'''
+"""
 BAEKJOON 12100번 문제
 https://www.acmicpc.net/problem/12100
 [ 입력 ]
@@ -7,14 +7,18 @@ https://www.acmicpc.net/problem/12100
 블록에 쓰여 있는 수는 2보다 크거나 같고, 1024보다 작거나 같은 2의 제곱꼴이다. 블록은 적어도 하나 주어진다.
 [ 출력 ]
 최대 5번 이동시켜서 얻을 수 있는 가장 큰 블록을 출력하라
-'''
+"""
 import sys
+from itertools import chain
 from enum import Enum, auto
 
 
 class DirType(Enum):
     HORIZONTAL = auto()
     VERTICAL = auto()
+    FORWARD = auto()
+    BACKWARD = auto()
+
 
 class GameMove(Enum):
     L = (-1, 0)
@@ -30,67 +34,112 @@ class GameMove(Enum):
     def type(self):
         if self in (GameMove.L, GameMove.R):
             return DirType.HORIZONTAL
-        else:  # GameMove.U or GameMove.D
+        else:  # GameMove.U | GameMove.D
             return DirType.VERTICAL
+
+    @property
+    def incr_or_decr(self):
+        if self in (GameMove.R, GameMove.D):
+            return DirType.FORWARD
+        else:  # GameMove.L | GameMove.U
+            return DirType.BACKWARD
 
 
 class SquareMatrix:
-
     def __init__(self, matrix):
         self._m = matrix
         self.size = len(matrix)
 
-    def get(x, y):
+    def get(self, x, y):
         return self._m[y][x]
 
-    def set(x, y, v):
+    def set(self, x, y, v):
         self._m[y][x] = v
 
-    def valid_index(index):
-        if index < 0 or index >= self.size:
-            return False
-        else:
-            return True
+    def max(self):
+        return max(chain(*self._m))
+
+    def __eq__(self, other):
+        return True if self._m == other._m else False
+
+    def __repr__(self):
+        return str(self._m)
+
+    def __str__(self):
+        return "\n".join((" ".join(map(str, row)) for row in self._m))
 
 
 class GameBoard(SquareMatrix):
-
     def __init__(self, initial_board):
         super().__init__(initial_board)
 
-    def iter_order(self, dir: GameMove):  # 안이쁜디
-        index_range = range(self.size)
+    def copy(self):
+        m = [row[:] for row in self._m]
+        return GameBoard(m)
 
-        if sum(dir.value) > 0:
-            index_range = reversed(index_range)
+    def iter_order(self, dir: GameMove):
+        """Generates an iterator by dir"""
 
-        for j in index_range:
-            if dir.type == DirType.HORIZONTAL:
-                yield from ((i, j) for i in index_range)
-            else:  # dir.type == DirType.VERTICAL
-                yield from ((j, i) for i in index_range)  # 이 반복 어떻게 없앱니까
-    
+        def sort(i, j):
+            return (i, j) if dir.type == DirType.HORIZONTAL else (j, i)
+
+        def order(indices):
+            return (
+                indices if dir.incr_or_decr == DirType.BACKWARD else reversed(indices)
+            )
+
+        indices = range(self.size)
+        for j in indices:
+            yield (sort(i, j) for i in order(indices))
+
     def next(self, dir: GameMove):
-        dx, dy = dir.value
-        for x, y in self.iter_order(dir):
-            if self.get(x, y) == 0:
-                continue
-            i, j = x, y
-            while self.valid_index(i) and self.valid_index(j):
-                pass
-            
+        board = self.copy()
+        dx, dy = dir.iter_increment
+        it = board.iter_order(dir)
+        for row in it:
+            for i, (x, y) in enumerate(row):
+                value = board.get(x, y)
+                # first element
+                if i == 0:
+                    m, n, v = x, y, value
+                    continue
+                # empty square
+                if not value:
+                    continue
+                # same as previous value => merge
+                if value == v:
+                    board.set(m, n, v * 2)
+                    board.set(x, y, 0)
+                    m, n, v = m + dx, n + dy, 0
+                # not empty, but different to previous value
+                elif value != 0:
+                    if v != 0:
+                        m, n = m + dx, n + dy
+                    v = value
+                    if (m, n) == (x, y):
+                        continue
+                    board.set(m, n, value)
+                    board.set(x, y, 0)
+        return board
 
 
 def solution(board):
-    game0 = GameBoard(board)
-    it = game0.iter_order(GameMove.D)
-    for v in it:
-        print(v)
-    return 0
+    """ A DFS-way solution using recursion """
+    def game_simulate(board, count):
+        if count == 5:
+            return board.max()
+        max_block = board.max()
+        for dir in GameMove:
+            if (next_state := board.next(dir)) != board:
+                max_block = max(max_block, game_simulate(next_state, count+1))
+        return max_block
+
+    initial_game = GameBoard(board)
+    return game_simulate(initial_game, 0)
 
 
 if __name__ == "__main__":
     readl = sys.stdin.readline
     n = int(readl())
-    board = [map(int, readl().split()) for _ in range(n)]
+    board = [list(map(int, readl().split())) for _ in range(n)]
     print(solution(board))
