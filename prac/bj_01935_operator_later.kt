2eaf5861d79a9ebcca37412dbd9c0f2141da55a6
mkkim <mk0218@postech.ac.kr>
2023-09-02 07:21:33 +0900

    .gitignore modified, unnecessary files deleted.

--- prac/bj_01935_operator_later.kt
+++ prac/bj_01935_operator_later.kt
@@ -1,29 +1,27 @@
 /* https://www.acmicpc.net/problem/1935 */
+
+
 fun main() {
     val n = readLine()!!.toInt()
     val exp = readLine()!!
     val operands = DoubleArray(n) { readLine()!!.toDouble() }
     val stack = Stack(100)
 
-    fun eval(op: Char) {
-        val b: Double = stack.pop()
-        val a: Double = stack.pop()
-        stack.push(when (op) {
-            '+' -> a + b
-            '-' -> a - b
-            '*' -> a * b
-            '/' -> a / b
-            else -> 0.00
-        })
-    }
-
     for (c in exp) {
         when (c) {
-            '+', '-', '*', '/' -> eval(c)
+            '+', '-', '*', '/' -> {
+                val b: Double = stack.pop()
+                val a: Double = stack.pop()
+                stack.push(when (c) {
+                    '+' -> a + b
+                    '-' -> a - b
+                    '*' -> a * b
+                    else -> a / b   // '/'
+                })
+            }
             else -> stack.push(operands[indexOf(c)])
         }
     }
-
     println("%.2f".format(stack.pop()))
 }
 
