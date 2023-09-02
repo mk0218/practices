b0ee85afbc66ba90c935b45ac41d6b8a37681084
mkkim <mk0218@postech.ac.kr>
2023-09-03 01:13:04 +0900

    https://www.acmicpc.net/problem/2075 (Kotlin) revised.

--- prac/bj_02075_nth_biggest.kt
+++ prac/bj_02075_nth_biggest.kt
@@ -1,49 +1,27 @@
 /* https://www.acmicpc.net/problem/2075 */
+import kotlin.math.min
 
 fun main() {
     val n = readLine()!!.toInt()
     val arr = DescArr(n)
-    var numbers = Array<IntArray>(n) { IntArray(n) { -1000000001 } }
-
-    numbers.map {
-        readLine()!!.split(" ").mapIndexed { i, v -> it[i] = v.toInt() }
-    }
-
-    for (j in n - 1 downTo 0) {
-        for (i in (0..n - 1)) {
-            arr.push(numbers[j][i])
-        }
-    }
-    println(arr.stack.arr[n - 1])
+    var numbers = List<List<Int>>(n) { readLine()!!.split(" ").map { it.toInt() }}
+    numbers.asReversed().forEach { it.forEach { v: Int -> arr.push(v) }}
+    println(arr.arr[n - 1])
 }
 
 class DescArr(maxSize: Int) {
     val maxSize = maxSize
-    var stack = Stack(maxSize)
-    var buffer = Stack(maxSize)
-    
-    fun push(v: Int): Boolean {
-        if (stack.size == maxSize && stack.head() > v) {
-            return false
-        } else {
-            while (stack.size > 0 && stack.head() < v) {
-                buffer.push(stack.pop())
-            }
-            stack.push(v)
-            while (stack.size < maxSize && buffer.size > 0) {
-                stack.push(buffer.pop())
-            }
-            buffer.clear()
-        }
-        return true
-    }
-}
-
-class Stack(maxSize: Int) {
+    var arr = IntArray(maxSize + 1) { -1000000001 }
     var size = 0
-    var arr = IntArray(maxSize)
-    val push = { v: Int -> arr[size++] = v }
-    val pop = {  arr[--size] }
-    val head = { arr[size - 1] }
-    val clear = { size = 0 }
+
+    fun push(v: Int) {
+        if (arr[maxSize - 1] <= v) {
+            var idx = size
+            while (idx > 0 && v > arr[idx - 1]) {
+                arr[idx] = arr[--idx]
+            }
+            arr[idx] = v
+            size = min(size + 1, maxSize - 1)
+        }
+    }
 }
